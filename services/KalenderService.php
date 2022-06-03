58b2b29e4ab63855bfe2dedb3cc583e0f0952d37
Max <max@localhost>
2022-06-03 15:06:35 +0200

    service

--- services/KalenderService.php
+++ services/KalenderService.php
@@ -1,11 +1,13 @@
 <?php
-include 'DBAccessManager.php';
+include_once 'DBAccessManager.php';
 
 class KalenderService extends DBAccessManager {
 
     const INSERT_KALENDER = "INSERT INTO QRKALENDER.KALENDER (BEZEICHNUNG, EMPFAENGER) VALUES (?, ?)";
     const SELECT_ALL_KALENDER = "SELECT K.ID, K.BEZEICHNUNG, K.EMPFAENGER FROM QRKALENDER.KALENDER K";
-    const SELECT_KALENDER_BY_ID = "SELECT K.ID, K.BEZEICHNUNG, K.EMPFAENGER FROM QRKALENDER.KALENDER K WHERE K.ID = ?"
+    const SELECT_KALENDER_BY_ID = "SELECT K.ID, K.BEZEICHNUNG, K.EMPFAENGER FROM QRKALENDER.KALENDER K WHERE K.ID = ?";
+    const UPDATE_KALENDER_BY_ID = "UPDATE QRKALENDER.KALENDER K SET K.BEZEICHNUNG = ?, K.EMPAENGER = ? WHERE K.ID = ?";
+    const DELETE_KALENDER_BY_ID = "DELETE FROM QRKALENDER.KALENDER K WHERE K.ID = ?";
 
     function __construct() {
         parent::__construct();
@@ -22,10 +24,12 @@ class KalenderService extends DBAccessManager {
         $stmt = $con->prepare(self::INSERT_KALENDER);
 		if (!$stmt->bind_param("ss", $bezeichnung, $empfaenger)) {
 			echo $stmt->error;
+            die;
 		}
 		
 		if (!$stmt->execute()) {
 			echo $stmt->error;
+            die;
 		}
 		
 		$stmt->close();
@@ -46,10 +50,12 @@ class KalenderService extends DBAccessManager {
         $stmt = $con->prepare(self::SELECT_KALENDER_BY_ID);
 		if (!$stmt->bind_param("d", $id)) {
 			echo $stmt->error;
+            die;
 		}
 		
 		if (!$stmt->execute()) {
 			echo $stmt->error;
+            die;
 		}
 
         $stmt->store_result();
@@ -57,6 +63,7 @@ class KalenderService extends DBAccessManager {
         if ($rows == 1) {
             if (!$stmt->bind_result($id, $bezeichnung, $empfaenger)) {
                 echo $stmt->error;
+                die;
             }
 
             $stmt->fetch(); // gefundene Zeile
@@ -86,6 +93,7 @@ class KalenderService extends DBAccessManager {
 		
 		if (!$stmt->execute()) {
 			echo $stmt->error;
+            die;
 		}
 
         $stmt->store_result();
@@ -93,6 +101,7 @@ class KalenderService extends DBAccessManager {
         if ($rows > 0) {
             if (!$stmt->bind_result($id, $bezeichnung, $empfaenger)) {
                 echo $stmt->error;
+                die;
             }
             $stmt->fetch(); // erste Zeile
             for ($i = 0; i < $rows; $i++) {
@@ -110,5 +119,55 @@ class KalenderService extends DBAccessManager {
 		$con->close();
 
         return $allKalender;
+    }
+
+    public function updateKalender($id, $bezeichnung, $empfaenger) {
+        $con = parent::getConnection();
+        if (!$con) {
+			echo 'Cannot connect to database';
+            die;
+		}
+
+        $stmt = $con->prepare(self::UPDATE_KALENDER_BY_ID);
+		
+		if (!$stmt->bind_param("ssd", $bezeichnung, $empfaenger, $id)) {
+			echo $stmt->error;
+            die;
+		}
+		
+		if (!$stmt->execute()) {
+			echo $stmt->error;
+            die;
+		}
+
+        $stmt->close();
+		$con->close();
+
+        echo 'success';
+    }
+
+    public function deleteKalender($id) {
+        $con = parent::getConnection();
+        if (!$con) {
+			echo 'Cannot connect to database';
+            die;
+		}
+
+        $stmt = $con->prepare(self::DELETE_KALENDER_BY_ID);
+		
+		if (!$stmt->bind_param("d", $id)) {
+			echo $stmt->error;
+            die;
+		}
+		
+		if (!$stmt->execute()) {
+			echo $stmt->error;
+            die;
+		}
+
+        $stmt->close();
+		$con->close();
+
+        echo 'success';
     }
 }
---
+++ services/ServiceInterface.php
@@ -0,0 +1,52 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+
+include_once 'KalenderService.php';
+
+// URI zerlegen, um später festzustellen, was aufgerufen werden soll
+$request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri_parts = explode('/', $request_uri);
+//print_r($uri_parts);
+
+$method = $_SERVER['REQUEST_METHOD'];
+switch ($method) {
+    case 'GET':
+        handleGET($uri_parts);
+        break;
+    case 'POST':
+        handlePOST($uri_parts);
+        break;
+    case 'PUT':
+        handlePUT($uri_parts);
+        break;
+    case 'DELETE':
+        handleDELETE($uri_parts);
+        break;
+    default:
+        break;
+}
+
+function handleGET($uri_parts) {
+    // ID kommt immer nach dem keyword in der URI
+    if (in_array('kalender', $uri_parts)) {
+        $id_index = array_search('kalender', $uri_parts) + 1;
+    } else if (in_array('eintrag')) {
+        $id_index = array_search('eintrag', $uri_parts) + 1;
+    }
+}
+
+function handlePOST($uri_parts) {
+
+}
+
+function handlePUT($uri_parts) {
+
+}
+
+function handleDELETE($uri_parts) {
+
+}
