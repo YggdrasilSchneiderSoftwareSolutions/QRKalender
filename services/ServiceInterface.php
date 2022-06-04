68ca93777f0cccbe598a094f682158a17c182274
Max <max@localhost>
2022-06-04 12:09:54 +0200

    Interface GET update

--- services/ServiceInterface.php
+++ services/ServiceInterface.php
@@ -6,6 +6,7 @@ header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 
 include_once 'KalenderService.php';
+include_once 'EintragService.php';
 
 // URI zerlegen, um später festzustellen, was aufgerufen werden soll
 $request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
@@ -31,9 +32,10 @@ switch ($method) {
 }
 
 function handleGET($uri_parts) {
-    if (in_array('kalender', $uri_parts)) {
+    // Es ist nur 'kalender' in der URI: GET /kalender/4711
+    if (in_array('kalender', $uri_parts) && !in_array('eintrag', $uri_parts)) {
         $kalenderService = new KalenderService();
-        $id_index = array_search('kalender', $uri_parts);
+        $id_index = array_search('kalender', $uri_parts); // FIXME Fall kann gar nicht eintreten
         if ($id_index === false) { // keine ID vorhanden -> getAll
             $result = $kalenderService->getAllKalender();
             echo json_encode($result);
@@ -42,7 +44,17 @@ function handleGET($uri_parts) {
             echo json_encode($result);
         }
     } else if (in_array('eintrag')) {
-        
+        $eintragService = new EintragService();
+        // GET auf alle Einträge eines Kalenders /kalender/4711/eintrag
+        if (in_array('kalender', $uri_parts)) {
+            $id_index = array_search('kalender', $uri_parts);
+            $result = $eintragService->getAllEintragForKalender(++$id_index);
+            echo json_encode($result);
+        } else { // GET nur ein bestimmten Eintrag /eintrag/4711
+            $id_index = array_search('eintrag', $uri_parts);
+            $result = $eintragService->getEintrag(++$id_index);
+            echo json_encode($result);
+        }
     }
 }
 
