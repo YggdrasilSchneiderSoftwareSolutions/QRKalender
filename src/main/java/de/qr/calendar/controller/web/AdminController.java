d51d6ba24c56cb4268e2623fb3cc92fa28353258
Max <max@localhost>
2022-11-17 16:06:56 +0100

    Eintrag update

--- src/main/java/de/qr/calendar/controller/web/AdminController.java
+++ src/main/java/de/qr/calendar/controller/web/AdminController.java
@@ -7,14 +7,15 @@ import de.qr.calendar.repository.EintragRepository;
 import de.qr.calendar.repository.KalenderRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.nio.file.FileSystems;
-import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.UUID;
 import java.util.stream.Collectors;
--- src/main/java/de/qr/calendar/controller/web/EintragController.java
+++ src/main/java/de/qr/calendar/controller/web/EintragController.java
@@ -1,13 +1,19 @@
 package de.qr.calendar.controller.web;
 
+import de.qr.calendar.file.FileService;
 import de.qr.calendar.model.Eintrag;
 import de.qr.calendar.repository.EintragRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.nio.file.FileSystems;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Slf4j
@@ -15,12 +21,32 @@ import java.util.UUID;
 @RequestMapping(path = "/eintrag")
 public class EintragController {
 
-    @Autowired
     private EintragRepository eintragRepository;
+    @Autowired
+    private FileService fileService;
+
+    @Autowired
+    public EintragController(EintragRepository eintragRepository) {
+        this.eintragRepository = eintragRepository;
+    }
 
     @GetMapping("/qr")
-    public String getEintragByScannedQrCode(@RequestParam(name = "id") String id, Model model) {
-        model.addAttribute("id", id);
+    public String getEintragByScannedQrCode(@RequestParam(name = "id") UUID id, Model model) {
+        log.info("QR-Code gescannt für Eintrag-ID {}", id);
+        LocalDate heute = LocalDate.now();
+        model.addAttribute("today", heute);
+
+        Eintrag eintrag = eintragRepository.findById(id).get();
+        model.addAttribute("eintrag", eintrag);
+        model.addAttribute("kalender", eintrag.getKalender());
+
+        String zielDatei = eintrag.getKalender().getId().toString()
+                + FileSystems.getDefault().getSeparator()
+                + eintrag.getBild();
+        // Datei als Resource laden
+        Resource resource = fileService.getFileAsResource(zielDatei);
+        model.addAttribute("eintragImage", resource);
+
         return "eintrag";
     }
 }
